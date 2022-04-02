b56ea04b2be8de00ea8a7cde08aa96692ea6158d
fosa-ij <efosaij@gmail.com>
2022-04-02 19:10:11 +0100

    new code wars challenge

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-// Beginner - Lost Without a Map
-// Given an array of integers, return a new array with each value doubled.
-// For example:
-// [1, 2, 3] --> [2, 4, 6]
+// A Needle in the Haystack
 
-//SOLUTION 
-let maps = arr => arr.map(x => x *= 2)
-console.log(maps([1, 2, 3]))
-// **********************************************************************
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+// should return "found the needle at position 5" (in COBOL "found the needle at position 6")
+
+// SOLUTION
+
+function findNeedle(haystack) {
+    let item = haystack.find(x => x === 'needle')
+    let itemIndex = haystack.findIndex(x => x === 'needle')
+    return `found the needle at position ${itemIndex}`
+  }
+  console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]))
