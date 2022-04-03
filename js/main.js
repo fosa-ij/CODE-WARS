e2bfae6f1c1cbb7670221c2c805cc5705b8f082d
fosa-ij <efosaij@gmail.com>
2022-04-03 23:55:03 +0100

    new code wars challenge

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// Square(n) Sum
-// Complete the square sum function so that it squares each number passed into it and then sums the results together.
-// For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+// Remove String Spaces
+// Simple, remove the spaces from the string, then return the resultant string
 
 
 // SOLUTION
-function squareSum(numbers){
-    return numbers.map(x => Math.pow(x, 2)).reduce((acc, curr) => acc + curr, 0)
+
+function noSpace(x){
+    return x.split(' ').join('')
     }
-console.log(squareSum([3, 4, 5]))
+    
