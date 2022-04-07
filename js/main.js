fd6d735f83ab29822c49e84b7c506b49ae9d8dea
fosa-ij <efosaij@gmail.com>
2022-04-07 16:37:26 +0100

    new CW --You only need one - Beginner

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-// Is there a vowel in there?
+// You only need one - Beginner
 
-// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
-// If they are, change the array value to a string of that vowel.
-// Return the resulting array.
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
 
-// SOLUION
-function isVow(a){
-// 97-a, 101-e, 105-i, 111-o, 117-u
-        let mapped = a.map(x => x === 97 ? 'a' : x === 101 ? 'e' : x === 105 ? 'i' : x === 111 ? 'o' : x === 117 ? 'u' : x)
-        return mapped
-      }
-    console.log(isVow([101,121,110,113,113,103,121,121,101,107,103]))
+// SOLUTION
+function check(a, x) {
+   return a.includes(x) ? true : false
+  }
+  console.log(check(['t', 'e', 's', 't'], 's'))
