cd0fee3706012bccb7dbb3515235a2b8c218ade3
fosa-ij <efosaij@gmail.com>
2022-04-07 17:06:57 +0100

    new CW --Well of Ideas - Easy Version

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-// Fake Binary
+// Well of Ideas - Easy Version
 
-// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
-// Note: input will never be an empty string
+// For every good kata idea there seem to be quite a few bad ones!
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'.
+// If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'.
+// If there are no good ideas, as is often the case, return 'Fail!'.
 
 // SOLUTION
-function fakeBin(x){
-    let zeroAndOne = x.split('').map(x => x < 5 ? x = 0 : x = 1).join('')
-    return zeroAndOne
-}
-console.log(fakeBin('45385593107843568'))
+function well(x){
+    if (x.includes('good')){
+        let goodFilters = x.filter(x => x === 'good')
+        return goodFilters.length <= 2 ? 'Publish!' : 'I smell a series!'
+      } else return 'Fail!'
+    }
+    console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good']))
