1a9bbdb3031662acbc27f6d614a98725077a2c6d
fosa-ij <efosaij@gmail.com>
2022-04-10 20:21:58 +0100

    new CW --Short Long Short

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
-// Name Shuffler
+// Short Long Short
 
-// Write a function that returns a string in which firstname is swapped with last name.
-// nameShuffler('john McClane'); => "McClane john"
+// Given 2 strings, a and b, return a string of the form short+long+short, 
+// with the shorter string on the outside and the longer string on the inside. 
+// The strings will not be the same length, but they may be empty ( zero length ).
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+// ("1", "22") --> "1221"
+// ("22", "1") --> "1221"
 
 // SOLUTION
-function nameShuffler(str){
-  //Shuffle It
-  let Shuffler = str.split(' ').reverse().join(' ')
-  return Shuffler
-}
-console.log(nameShuffler('john McClane'))
+function solution(a, b){
+  return a.length > b.length ? (b + a + b) : (a + b + a)
+  }
+ console.log(solution('13', '200'))
