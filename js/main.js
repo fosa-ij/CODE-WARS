56398a327cf0efe8f8b54fc55ca8930ae921677f
fosa-ij <efosaij@gmail.com>
2022-04-11 23:37:49 +0100

    new CW --Function 3 - multiplying two numbers

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
-// How good are you really?
+// Function 3 - multiplying two numbers
 
-// There was a test in your class and you passed it. Congratulations!
-// But you're an ambitious person. You want to know if you're better than the average student in your class.
-// You receive an array with your peers' test scores. Now calculate the average and compare your score!
-// Return True if you're better, else False!
-
-// Note:
-// Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+// Implement a function which multiplies two numbers.
 
 // SOLUTION
-function betterThanAverage(classPoints, yourPoints) {
-  classPoints.push(yourPoints)
-  let total = classPoints.reduce((acc, curr) => acc + curr, 0)
-  let avg = total / classPoints.length
-  return yourPoints > avg 
-}
-console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75))
+const multiply = (a, b) => a * b
