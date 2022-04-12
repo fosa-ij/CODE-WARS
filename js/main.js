becfce833eb4d867794d28e9d29b91651abcd76c
fosa-ij <efosaij@gmail.com>
2022-04-12 23:26:29 +0100

    new CW --

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
-// Function 3 - multiplying two numbers
+// Count Odd Numbers below n
 
-// Implement a function which multiplies two numbers.
+// Given a number n, return the number of positive odd numbers below n, EASY!
+
+// Examples (Input -> Output)
+// * 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+// * 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+// Expect large Inputs!
 
 // SOLUTION
-const multiply = (a, b) => a * b
+console.log(oddCount(n)); 
+function oddCount(n){
+    return Math.floor(n/2)
+  }
