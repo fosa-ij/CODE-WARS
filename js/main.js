11c65969d79a2154d29087c1cff355c0b2db63de
fosa-ij <efosaij@gmail.com>
2022-04-13 22:55:53 +0100

    new CW --To square(root) or not to square(root)

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-// Count Odd Numbers below n
+// To square(root) or not to square(root)
 
-// Given a number n, return the number of positive odd numbers below n, EASY!
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+// Return a new array with processing every number of the input-array like this:
+// If the number has an integer square root, take this, otherwise square the number.
 
-// Examples (Input -> Output)
-// * 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
-// * 15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
-// Expect large Inputs!
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only positive numbers, and will never be empty or null.
 
 // SOLUTION
-console.log(oddCount(n)); 
-function oddCount(n){
-    return Math.floor(n/2)
-  }
+function squareOrSquareRoot(array) {
+  return array.map(x => Number.isInteger(Math.sqrt(x)) ? Math.sqrt(x) : Math.pow(x, 2))
+}
+console.log(squareOrSquareRoot([ 1, 2, 3, 4, 5, 6 ]))
