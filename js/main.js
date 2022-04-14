49a5a15839068d28504d17233c28cbf0700d2c98
fosa-ij <efosaij@gmail.com>
2022-04-14 20:37:36 +0100

    new CW --Adding Arrays

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
-// Filter unused digits
+// Adding Arrays
 
-// Given a varying number of integer arguments, return the digits that are not present in any of them.
+// Create a function that takes an array of letters, and combines them into words in a sentence.
 
-// Example:
-// [12, 34, 56, 78]  =>  "09"
-// [2015, 8, 26]     =>  "3479"
-// Note: the digits in the resulting string should be sorted
+// The array will be formatted as so:
 
-// SOLUTION
-function unusedDigits(...x) {
-  const a = [0,1,2,3,4,5,6,7,8,9]
-  let y = x.join('')
-  let newArr = a.filter(arr => !y.includes(arr))
-  return newArr.join('');
+// [['J','L','L','M']
+// ,['u','i','i','a']
+// ,['s','v','f','n']
+// ,['t','e','e','']]
+// The function should combine all the 0th indexed letters to create the word 'just', all the 1st indexed letters to create the word 'live', etc.
+
+// Shorter words will have an empty string in the place once the word has already been mapped out (see the last element in the last element in the array).
+
+// Examples:
+
+// arrAdder([
+// ['J','L','L','M'],
+// ['u','i','i','a'],
+// ['s','v','f','n'],
+// ['t','e','e','']
+// ]) // => "Just Live Life Man"
+
+// arrAdder([ 
+//   [ 'T', 'M', 'i', 't', 'p', 'o', 't', 'c' ],
+//   [ 'h', 'i', 's', 'h', 'o', 'f', 'h', 'e' ],
+//   [ 'e', 't', '', 'e', 'w', '', 'e', 'l' ],
+//   [ '', 'o', '', '', 'e', '', '', 'l' ],
+//   [ '', 'c', '', '', 'r', '', '', '' ],
+//   [ '', 'h', '', '', 'h', '', '', '' ],
+//   [ '', 'o', '', '', 'o', '', '', '' ],
+//   [ '', 'n', '', '', 'u', '', '', '' ],
+//   [ '', 'd', '', '', 's', '', '', '' ],
+//   [ '', 'r', '', '', 'e', '', '', '' ],
+//   [ '', 'i', '', '', '', '', '', '' ],
+//   [ '', 'a', '', '', '', '', '', '' ] ]) // => "The Mitochondria is the powerhouse of the cell"
+
+//   SOLUTION
+function arrAdder(arr) {
+  return arr.reduce((acc,curr) => acc.map((a,b) => a + curr[b])).join(' ')
 }
-console.log(unusedDigits(12,34,56,78))
+console.log(arrAdder([
+['J','L','L','M'],
+['u','i','i','a'],
+['s','v','f','n'],
+['t','e','e','']
+]))
