185b4e395505aac77c113948d62609dcb490ce70
fosa-ij <efosaij@gmail.com>
2022-04-25 19:36:40 +0100

    new EQ.js --The sum of a range

--- js/main.js
+++ js/main.js
@@ -1,30 +1,22 @@
-// Transportation on vacation
+// The sum of a range
+// The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
 
-// After a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+// console.log(sum(range(1, 10)));
 
-// You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
 
-// Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
 
-// Write a code that gives out the total amount for different days(d).
+// As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
 
 // SOLUTION
-function baseCost(days, rate) {
-  return days * rate;
-}
-
-function discountRate(days) {
-  if ( days >= 7 ) {
-    return 50;
+let range = (start, end, iteration = 1) => {
+  let rangei = []
+  for (let i = start; i <= end; i += iteration){
+    rangei.push(i)
   }
-  else if ( days >= 3 ) {
-    return 20;
-  }
-  else {
-    return 0;
-  }
+  return rangei
 }
-
-function rentalCarCost(days) {
-  return baseCost(days, 40) - discountRate(days);
-}
+console.log(range(1, 10, 3))
+let sum = arr => arr.reduce((acc, curr) => acc + curr, 0)
+console.log(sum(range(1, 10, 3)))
