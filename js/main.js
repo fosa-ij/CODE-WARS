3b60b90730a7b026a864af663bb500c7d21a0ed2
fosa-ij <efosaij@gmail.com>
2022-04-25 21:51:09 +0100

    new CW --Switch/Case - Bug Fixing #6

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
-// The sum of a range
-// The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
+// Switch/Case - Bug Fixing #6
 
-// console.log(sum(range(1, 10)));
-
-// Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
-
-// Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the example program and see whether it does indeed return 55.
-
-// As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used when building the array. If no step is given, the elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
+// Switch/Case - Bug Fixing #6
+// Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
 
 // SOLUTION
-let range = (start, end, iteration = 1) => {
-  let rangei = []
-  for (let i = start; i <= end; i += iteration){
-    rangei.push(i)
+function evalObject(value){
+  var result = 0;
+  switch(value.operation){
+    case'+': result = value.a + value.b;
+      break
+    case'-': result = value.a - value.b;
+      break
+    case'/': result = value.a / value.b;
+      break
+    case'*': result = value.a * value.b;
+      break
+    case'%': result = value.a % value.b;
+      break
+    case'^': result = Math.pow(value.a, value.b);
+      break
   }
-  return rangei
+  return result;
 }
-console.log(range(1, 10, 3))
-let sum = arr => arr.reduce((acc, curr) => acc + curr, 0)
-console.log(sum(range(1, 10, 3)))
