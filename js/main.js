b45cffad19658722a2a4ed201387994161421f2b
fosa-ij <efosaij@gmail.com>
2022-05-01 22:46:26 +0100

    new CW --Enumerable Magic #1 - True for All?

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-// Regular Ball Super Ball
+// Enumerable Magic #1 - True for All?
 
-// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
-// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+// Task
+// Create a method all which takes two params:
 
-// ball1 = new Ball();
-// ball2 = new Ball("super");
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
 
-// ball1.ballType     //=> "regular"
-// ball2.ballType     //=> "super"
+// Example
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
 
 // SOLUTION
-class Ball {
-  constructor(ballType = 'regular'){
-    this.ballType = ballType
-  }
+function all(arr, func){
+  //if the filtered array is equal in length to the original array return true
+  let filtered = arr.filter(func)
+  if (!arr) return true
+  return filtered.length === arr.length
 }
-let ball1 = new Ball();
-let ball2 = new Ball('super');
-console.log(ball1.ballType)
-console.log(ball2.ballType)
+console.log(all([1,2,3,4,5], function(v){return v > 9}))
