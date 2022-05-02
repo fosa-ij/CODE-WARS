c147ef12abc0be6daa6ad416873a205a61645f82
fosa-ij <efosaij@gmail.com>
2022-05-02 09:06:06 +0100

    new CW --Is it a palindrome?

--- js/main.js
+++ js/main.js
@@ -1,19 +1,10 @@
-// What is between?
+// Is it a palindrome?
 
-// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
-
-// For example:
-
-// a = 1
-// b = 4
-// --> [1, 2, 3, 4]
+// Write a function that checks if a given string (case insensitive) is a palindrome.
 
 // SOLUTION
-function between(a, b) {
+function isPalindrome(x) {
   // your code here
-  let arr = [];
-  for (let i = a; i <= b; i++){
-     arr.push(i)
-  }
-  return arr
+  let xL = x.toLowerCase();
+  return xL === xL.split('').reverse().join('')
 }
