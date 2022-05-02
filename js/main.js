776437544f8b7a6790f4eac60c6da63e4ffdb254
fosa-ij <efosaij@gmail.com>
2022-05-02 08:58:36 +0100

    new CW --What is between?

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-// Opposites Attract
+// What is between?
 
-// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 
-// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
 
 // SOLUTION
-function lovefunc(flower1, flower2){
-  // moment of truth
-    // return !(flower1 % 2 == 0 && flower2 % 2 == 0 || flower1 % 2 != 0 && flower2 % 2 != 0)
-    return flower1 % 2 !== flower2 % 2
+function between(a, b) {
+  // your code here
+  let arr = [];
+  for (let i = a; i <= b; i++){
+     arr.push(i)
   }
-console.log(lovefunc(1,4))
+  return arr
+}
