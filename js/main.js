86d4db653345c38c70c2ab3bdf09b3073d229b09
fosa-ij <efosaij@gmail.com>
2022-05-05 22:50:10 +0100

    new CW --Plural

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
-// String cleaning
+// Plural
 
-// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+// We need a simple function that determines if a plural is needed or not. It should take a number, and return true if a plural should be used with that number or false if not. This would be useful when printing out a string such as 5 minutes, 14 apples, or 1 sun.
 
-// Examples (input -> output)
-// '! !'                 -> '! !'
-// '123456789'           -> ''
-// 'This looks5 grea8t!' -> 'This looks great!'
-// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+// You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
+
+// All values will be positive integers or floats, or zero.
 
 // SOLUTION
-function stringClean(s){
-  // Function will return the cleaned string
-  return s.split('').filter((x, i) => x != parseInt(x)).join('')
-  
+function plural(n) {
+  // ...
+  return n != 1
 }
-console.log(stringClean("(E3at m2e2!0!)"))
