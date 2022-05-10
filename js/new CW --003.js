bdc95537fd3f1a425d800b7bd25220177971774f
fosa-ij <efosaij@gmail.com>
2022-05-10 22:29:16 +0100

    change input parameter to animal

--- js/new CW --003.js
+++ js/new CW --003.js
@@ -9,5 +9,5 @@
 // Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide
 
 // SOLUTION
-let mouthSize = input => input.toLowerCase() === 'alligator' ? 'small' : 'wide'
+let mouthSize = animal => animal.toLowerCase() === 'alligator' ? 'small' : 'wide'
 console.log(mouthSize('alligator'));
