6cfb32375f208fcbe0ba88e1145335f2df4a5271
fosa-ij <efosaij@gmail.com>
2022-05-27 21:12:10 +0100

    You Can't Code Under Pressure #1

---
+++ js/newCW--018.js
@@ -0,0 +1,10 @@
+/* You Can't Code Under Pressure #1
+
+Code as fast as you can! You need to double the integer and return it. */
+
+// SOLUTION
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+}
+console.log(doubleInteger(4)); 
