ffadde5a41125c6eee4a9b6e3747132bd87b61fe
fosa-ij <efosaij@gmail.com>
2022-06-09 23:28:42 +0100

    Duck Duck Goose

---
+++ js/cw--038.js
@@ -0,0 +1,24 @@
+/* Duck Duck Goose
+
+The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+
+SOLUTION */
+// P : two parameters, an array of objects and an integer 
+
+// R : a string 
+
+// E : duck_duck_goose([a, b, c, d], 1) should return a.name \\ duck_duck_goose([a, b, c, d], 5) should return a.name
+
+// P : so the gist is to loop through the array more than once using the second parameter, how?? one way to do it would be to create a for loop and when the i is more than the length of the array start from the first item in the array again OR you cann subtract the length of the array from the integer given or divide it some kinda way
+
+function duckDuckGoose(players, goose) {
+    // ...
+}
