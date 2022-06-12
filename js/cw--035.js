d7e6f439f1f100ae7e973fd02c4064d85e24f618
fosa-ij <efosaij@gmail.com>
2022-06-12 23:53:25 +0100

    Sort by Last Char

---
+++ js/cw--035.js
@@ -0,0 +1,20 @@
+/* Sort by Last Char
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+
+SOLUTION */
+// P : one (string of words) 
+
+// R : an array of words
+
+// E : last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']); || last('take me to semynak'), ['take', 'me', 'semynak', 'to']);  
+
+// P : the big question will be, how to sort in alphabetical order? || that's done by the sort method \\ 
+
+function last(x){
+}
+last('take me to semynak')
