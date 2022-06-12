4e247147f2422d5ea423bac46718c38de7876d2d
fosa-ij <efosaij@gmail.com>
2022-06-13 00:39:14 +0100

    Sort by Last Char

--- js/cw--035.js
+++ js/cw--035.js
@@ -13,8 +13,15 @@ SOLUTION */
 
 // E : last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']); || last('take me to semynak'), ['take', 'me', 'semynak', 'to']);  
 
-// P : the big question will be, how to sort in alphabetical order? || that's done by the sort method \\ 
+// P : the big question will be, how to sort in alphabetical order? || that's done by the localeCompare method \\ so basically what we want to do is use the method for only the last letter in the word
 
 function last(x){
+    let arr = x.split(' ')
+    console.log(arr[3].charAt(arr[3].length -1));
+    return arr.sort((a, b) => {
+        return (a.charAt(a.length -1)).localeCompare((b.charAt(b.length -1)))
+        // (a.slice(-1)).localeCompare(b.slice(-1))
+    })
 }
-last('take me to semynak')
+console.log(last('a b c den i ab z x p'));
+console.log(last('take me to semynak'));
