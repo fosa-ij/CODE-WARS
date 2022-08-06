2efeae207a271fb98eb12913bf7633bc5d3b865c
fosa-ij <efosaij@gmail.com>
2022-08-07 00:12:31 +0100

    Regex validate PIN code

--- js/cw--091.js
+++ js/cw--091.js
@@ -11,16 +11,23 @@
 
 function validatePIN (pin) {
     //return true or false
-    if (pin.length === 4 || pin.length ===  6){
-    //  so first condition should be to know if the string meets the condition of 4 or 6 numbers
-        const pinArr = pin.split("").map(x => Number(x))
-        console.log(pinArr);
-    //  so firstly convert to array and loop through to find if everything is a number(i suggest using the filter method to check and equate it to the original array)
-        const filterPinArr = pinArr.filter(x => Number.isInteger(x))
-        console.log(filterPinArr);
-        return pinArr.length === filterPinArr.length
-    } else return false
+    // if (pin.length === 4 || pin.length ===  6){
+    // //  so first condition should be to know if the string meets the condition of 4 or 6 numbers
+    //     const pinArr = pin.split("").map(x => Number(x))
+    //     console.log(pinArr);
+    // //  so firstly convert to array and loop through to find if everything is a number(i suggest using the filter method to check and equate it to the original array)
+    //     const filterPinArr = pinArr.filter(x => Number.isInteger(x))
+    //     console.log(filterPinArr);
+    //     return pinArr.length === filterPinArr.length
+    // } else return false
+    var n = pin.length;
+    if( n != 4 && n != 6)
+            return false;
+    for (var i in pin)
+            if (pin[i] > '9' || pin[i] < '0')
+                return false;
+    return true;
 }
-console.log(validatePIN("123."));
+console.log(validatePIN("123"));
 console.log(validatePIN("122345"));
 console.log(validatePIN("1234.4"));
